da01bfcfe5073433481b2eebb3006462c922b32c
CakesStuff <95542792+CakesStuff@users.noreply.github.com>
2023-12-01 15:48:00 +0100

    summe btree

--- _test/BinaryTreeTest.java
+++ _test/BinaryTreeTest.java
@@ -1,6 +1,4 @@
 package _test;
-import linear.List;
-import linear.ListWithViewer;
 import gui.GUI;
 import baeume.BinaryTree;
 import baeume.TreeViewer;
@@ -10,22 +8,22 @@ public class BinaryTreeTest {
 	public BinaryTree<Integer> suchbaum;
 	
 	public BinaryTreeTest(){
-        suchbaum = new BinaryTree<Integer>(59);
-        BinaryTree<Integer> b4= new BinaryTree<Integer>(4);
-        BinaryTree<Integer> b34 = new BinaryTree<Integer>(34);
-        BinaryTree<Integer> b16 = new BinaryTree<Integer>(16);
-        BinaryTree<Integer> b7= new BinaryTree<Integer>(7);
-        BinaryTree<Integer> b6 = new BinaryTree<Integer>(6);
-        BinaryTree<Integer> b12 = new BinaryTree<Integer>(12);
-        BinaryTree<Integer> b18= new BinaryTree<Integer>(18);
-        BinaryTree<Integer> b17= new BinaryTree<Integer>(17);
-        BinaryTree<Integer> b53 = new BinaryTree<Integer>(53);
-        BinaryTree<Integer> b45 = new BinaryTree<Integer>(45);
-        BinaryTree<Integer> b78 = new BinaryTree<Integer>(78);
-        BinaryTree<Integer> b62 = new BinaryTree<Integer>(62);
-        BinaryTree<Integer> b61= new BinaryTree<Integer>(61);
-        BinaryTree<Integer> b71 = new BinaryTree<Integer>(71);
-        BinaryTree<Integer> b73 = new BinaryTree<Integer>(73);
+        suchbaum = new BinaryTree<>(59);
+        BinaryTree<Integer> b4= new BinaryTree<>(4);
+        BinaryTree<Integer> b34 = new BinaryTree<>(34);
+        BinaryTree<Integer> b16 = new BinaryTree<>(16);
+        BinaryTree<Integer> b7= new BinaryTree<>(7);
+        BinaryTree<Integer> b6 = new BinaryTree<>(6);
+        BinaryTree<Integer> b12 = new BinaryTree<>(12);
+        BinaryTree<Integer> b18= new BinaryTree<>(18);
+        BinaryTree<Integer> b17= new BinaryTree<>(17);
+        BinaryTree<Integer> b53 = new BinaryTree<>(53);
+        BinaryTree<Integer> b45 = new BinaryTree<>(45);
+        BinaryTree<Integer> b78 = new BinaryTree<>(78);
+        BinaryTree<Integer> b62 = new BinaryTree<>(62);
+        BinaryTree<Integer> b61= new BinaryTree<>(61);
+        BinaryTree<Integer> b71 = new BinaryTree<>(71);
+        BinaryTree<Integer> b73 = new BinaryTree<>(73);
         
         suchbaum.setLeftTree(b4);
         b4.setRightTree(b34);
@@ -52,10 +50,8 @@ public class BinaryTreeTest {
 	
 
 	private int summe(BinaryTree<Integer> pTree) {
-		int ergebnis = 0;
-		// TODO programmieren:
-		// Abbruchbedingung, Wurzelbehandlung, 2 rekursive Aufrufe, Sachlogik
-		return ergebnis;
+		if(pTree == null || pTree.isEmpty())return 0;
+		return pTree.getContent() + summe(pTree.getLeftTree()) + summe(pTree.getRightTree());
 	}
 
 	public static void main(String[] args) {
